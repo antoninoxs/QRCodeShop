107db5c7b2d4312118e3f179dc9189c76502b13b
Costantino <costantino.depetrillo@gmail.com>
2013-11-21 15:40:20 +0100

    GestitaSocketTimeout Exception

--- ShopApp/src/it/torvergata/mp/helper/HttpConnection.java
+++ ShopApp/src/it/torvergata/mp/helper/HttpConnection.java
@@ -135,13 +135,13 @@ public class HttpConnection {
 				handler.sendMessage(message);
 			}
 			else if(phpFile=="ordernew"){
-				Log.e("TIMEOUT", "Timeout connection: " + e.toString());
+				Log.e("TIMEOUT", "Connection Timeout: " + e.toString());
 				object = new JSONObject();
 				object.put("result", Const.TIMEOUT);
 				return object;
 			}
 			else{
-				Log.e("TIMEOUT", "Timeout connection: " + e.toString());
+				Log.e("TIMEOUT", "Socket Timeout: " + e.toString());
 				Message message = handler.obtainMessage(1, Const.TIMEOUT, 0);
 				handler.sendMessage(message);
 			}
@@ -156,114 +156,6 @@ public class HttpConnection {
 		return object;
 	
 	}
-	public JSONObject connect2(String phpFile, JSONObject json , Handler handler,int sockTimeout) throws JSONException{
-		// Preparazione delle informazioni da inviare al server
-	JSONObject object=null;
-		try {
-
-			// Connessione al Server
-
-			//Creazione oggetto per parametri
-			HttpParams httpParameters = new BasicHttpParams();
-			
-			// Set the timeout in milliseconds until a connection is established.
-			int timeoutConnection = 3000;
-
-			//Imposto il parametro di timeout connection
-			HttpConnectionParams.setConnectionTimeout(httpParameters, timeoutConnection);
-			
-			// Set the default socket timeout (SO_TIMEOUT) 
-			// in milliseconds which is the timeout for waiting for data.
-			int timeoutSocket = sockTimeout;
-			
-			//Imposto il timeout della socket
-			HttpConnectionParams.setSoTimeout(httpParameters, timeoutSocket);
-
-			
-			DefaultHttpClient httpClient = new DefaultHttpClient(httpParameters);
-				
-			
-			//********************
-			
-			URL url = new URL("http://" + Const.IPADDRESS
-					+ "/"+phpFile+".php");
-			
-			HttpPost httpPost = new HttpPost(url.toURI());
-			Log.i("PASSWORD PRIMA IN SET ENTITY",json.toString(4) );
-			// Prepare JSON to send by setting the entity
-			httpPost.setEntity(new StringEntity(json.toString(), "UTF-8"));
-
-			// Set up the header types needed to properly transfer JSON
-			httpPost.setHeader("Content-Type", "application/json");
-			httpPost.setHeader("Accept-Encoding", "application/json");
-			httpPost.setHeader("Accept-Language", "en-US");
-
-			// Execute POST
-			//HttpResponse response = httpClient.execute(httpPost);
-			
-			//**********************
-			Log.i("Json Inviato: ", json.toString(4));
-			
-			// Ricezione della risposta
-			BasicHttpResponse httpResponse = (BasicHttpResponse)  httpClient.execute(httpPost);
-			
-			// Conersione da inputString a JsonResult
-			String jsonResult = GenericFunctions.inputStreamToString(
-					httpResponse.getEntity().getContent()).toString();
-			Log.i("JsonResult", "[" + jsonResult + "]");
-			
-			object = new JSONObject(jsonResult);
-			
-			return object;
-			
-		}catch (ConnectTimeoutException e) {
-			if (phpFile=="registrazione")
-			{
-				//Comunicazione al Thread principale dell'esito dell'operazione di Registrazione
-				Message message = handler.obtainMessage();
-				Bundle b = new Bundle();
-				b.putString("Message","");
-				b.putString("Result", Const.TIMEOUTS);
-				b.putString("errorQuery","");
-				message.setData(b);
-				handler.sendMessage(message);
-			}
-			else{
-				
-				//Message message = handler.obtainMessage(1, Const.TIMEOUT, 0);
-				//handler.sendMessage(message);
-			}
-			
-		} catch (SocketTimeoutException e) {
-			if (phpFile=="registrazione")
-			{
-				//Comunicazione al Thread principale dell'esito dell'operazione di Registrazione
-				Message message = handler.obtainMessage();
-				Bundle b = new Bundle();
-				b.putString("Message","");
-				b.putString("Result", Const.TIMEOUTS);
-				b.putString("errorQuery","");
-				message.setData(b);
-				handler.sendMessage(message);
-			}
-			else{
-				Log.e("TIMEOUT", "Timeout connection: " + e.toString());
-				object = new JSONObject();
-				object.put("result", Const.TIMEOUT);
-				return object;
-				
-				//Message message = handler.obtainMessage(1, Const.TIMEOUT, 0);
-				//handler.sendMessage(message);
-			}
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			Log.e("log_tag", "Error in http crsaFdgsonnection: " + e.toString());
-		}
-		return object;
 	
-	}
 
 }
