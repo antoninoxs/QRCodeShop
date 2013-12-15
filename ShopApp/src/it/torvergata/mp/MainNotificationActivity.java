7690dd2452ad27d7530b49c5f887a1703a24a8c5
Costantino <costantino.depetrillo@gmail.com>
2013-12-15 16:45:33 +0100

    Notifiche Perfezionate

--- ShopApp/src/it/torvergata/mp/MainNotificationActivity.java
+++ ShopApp/src/it/torvergata/mp/MainNotificationActivity.java
@@ -29,7 +29,7 @@ public class MainNotificationActivity extends Activity {
    @Override
    public void onCreate(Bundle savedInstanceState){
        super.onCreate(savedInstanceState);
-       setContentView(R.layout.activity_main);
+       setContentView(R.layout.activity_notification);
        // Il telefono risulta pronto
        GCMRegistrar.checkDevice(this);
        // Il manifesto risulta pronto
